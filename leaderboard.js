6abd7870071572ecf392aba50cab14dc4614fdd8
Sashko Stubailo <sashko@mit.edu>
2014-10-14 16:03:53 -0700

    Remove avatars, code style

--- leaderboard.js
+++ leaderboard.js
@@ -40,7 +40,10 @@ if (Meteor.isServer) {
       var names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
                    "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
       _.each(names, function (name) {
-        Players.insert({name: name, score: Math.floor(Random.fraction()*10)*5});
+        Players.insert({
+          name: name,
+          score: Math.floor(Random.fraction() * 10) * 5
+        });
       });
     }
   });
